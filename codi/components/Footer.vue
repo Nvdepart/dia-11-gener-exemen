87e35ad8329d4297c1ff91be973eb1de4763cd47
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-01-11 17:01:48 +0100

    footer

---
+++ codi/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card>
+        <h2>Atlas-Expo</h2>
+        <div>
+            <v-btn v-for="(s, i) in sm" :key="i" icon>
+                <v-icon >
+                    {{ s.icon }}
+                </v-icon>
+            </v-btn>
+        </div>
+        <v-card-text></v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+        sm:[
+            {icon:'mdi-facebook', link:'https://www.facebook.com'},
+            {icon:'mdi-twitter', link:'https://www.twitter.com'},
+            {icon:'mdi-instagram', link:'https://www.instagram.com'},
+            {icon:'mdi-youtube', link:'https://www.youtube.com'},
+        ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
