f24715f6ae107480d48f5df34ea28c251ab472b7
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-01-11 16:50:16 +0100

    Insercio imatge

---
+++ codi/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+            <h1>Je suis la barre de menu</h1>
+            <v-app-bar
+                    :clipped-left="clipped"
+                    fixed
+                    app
+            >
+            <v-toolbar-title>
+                Atlas-Expo
+            </v-toolbar-title>
+            <v-spacer />
+            <v-btn icon>
+            <v-icon>mdi-home</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-account-box-outline</v-icon>
+            </v-btn>
+
+            </v-app-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ codi/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+       <v-img src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80">Je suis une photo</v-img>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
